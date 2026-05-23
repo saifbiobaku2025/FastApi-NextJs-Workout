811c806084b9735e2d5e996b89a8a2b6106c388d
saif biobaku <saifbiobaku22025@gmail.com>
2026-05-23 16:33:38 -0400

    Refactor authentication routes and improve code formatting in auth.py and main.py

--- fastapi/api/main.py
+++ fastapi/api/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers import auth, workouts, routines
-from api.routers import auth
 
 app = FastAPI()
 
@@ -11,16 +10,17 @@ Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
-    allow_credentials=True, 
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
 
 @app.get("/")
 def health_check():
     return {"message": "API is healthy!"}
 
+
 app.include_router(auth.router)
 app.include_router(workouts.router)
 app.include_router(routines.router)
-
